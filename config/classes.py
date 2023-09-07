4ecdd7b20f3e69956ae0e9918a5f625cd014c64d
Ibrahim WORK <work@mail.ri>
2023-09-07 17:01:39 +0300

    фикс кода, добавлено: миксин для сбора вакансий, to json, check_values, fix get_salary, add_vacancy, get_salary_for_print

--- config/classes.py
+++ config/classes.py
@@ -5,14 +5,21 @@ from abc import ABC, abstractmethod
 import requests
 
 
+class MixinVacanciesStorage:
+    """Класс для сбора вакансий в списке."""
+    collected_vacancies = []
+    vacancies_count = len(collected_vacancies)
+
+
 class Saver(ABC):
     """Абстрактный класс для сохранения вакансий в файл."""
-    json_file = 'config/vacancies.json'
+    json_file = 'data/vacancies.json'
+
     # csv_file = ''
     # txt_file = ''
 
     @abstractmethod
-    def to_json(self, json_file, vacancies):
+    def to_json(self, json_file):
         pass
 
     @staticmethod
@@ -21,67 +28,116 @@ class Saver(ABC):
             raise FileNotFoundError("JSON файл с вакансиями не найден")
 
 
-class JSONSaver(Saver):
+class JSONSaver(Saver, MixinVacanciesStorage):
     """Класс для сохранения вакансий в JSON формате."""
     __slots__ = ()
 
-    def to_json(self, json_file, vacancies):
-        print(self.json_file)
+    def to_json(self, vacancies: list) -> None:
+        """Метод добавления вакансий в JSON файл"""
+        with open(self.json_file, 'r', encoding='utf-8') as f:
+            vacancies_list = json.load(f)
+            for vacancy in vacancies:
+                vacancy = {"id": vacancy.vacancy_id,
+                           "name": vacancy.name,
+                           "salary": vacancy.salary,
+                           "vacancy_url": vacancy.vacancy_url,
+                           "description": vacancy.description,
+                           }
+
+                vacancies_list.append(vacancy)
+
+            with open(self.json_file, 'w', encoding='utf-8') as outfile:
+                json.dump(vacancies_list, outfile, ensure_ascii=False, indent=2)
 
 
-class Vacancy:
+class Vacancy(MixinVacanciesStorage):
     """Класс для работы с вакансиями."""
     __slots__ = ('vacancy_id', 'name', 'salary', 'vacancy_url', 'description')
 
-    collected_vacancies = []
-
     def __init__(self, vacancy_id: int, name: str, salary: str | dict, vacancy_url: str, description: str):
-        self.vacancy_id = vacancy_id
-        self.name = name
-        self.salary = self.__get_user_salary(salary)
-        self.vacancy_url = vacancy_url
-        self.description = description
+        if self.__check_values(vacancy_id, name, salary, vacancy_url, description):
+            self.vacancy_id = vacancy_id
+            self.name = name
+            self.salary = self.__get_salary(salary)
+            self.vacancy_url = vacancy_url
+            self.description = description
+
+    @classmethod
+    def __check_values(cls, vacancy_id: int, profession: str, salary: dict | str, vacancy_url: str,
+                       description: str) -> bool:
+        """Метод класса для возбуждения исключений при неверных входных данных."""
+        if not isinstance(vacancy_id, int):
+            raise TypeError("ID Вакансии должен состоять из цифр")
+        if not len(str(vacancy_id)) == 8:
+            raise ValueError("Длина ID вакансии должно быть равным 8 (восьми)")
+        if not len(profession) >= 10:
+            raise TypeError("Наименование вакансии должно состоять из минимум 10 символов.")
+        if isinstance(salary, dict):
+            if not salary["from"] or not salary["to"] or not salary["currency"]:
+                raise KeyError("Не указано значение зарплаты ОТ, ДО или ВАЛЮТА")
+        elif isinstance(salary, int):
+            raise "Зарплата ОТ, ДО и Валюта должны быть разделены пробелами (всего два пробела)"
+        elif salary != "Не указана":
+            salary_split = salary.split()
+            if len(salary_split) != 3:
+                raise "Зарплата ОТ, ДО и Валюта должны быть разделены пробелами (всего два пробела)"
+            elif not int(salary_split[1]) >= int(salary_split[0]):
+                raise "Зарплата ДО должна быть больше или равна зарплате ОТ"
+            elif len(salary_split[2]) != 3:
+                raise "Наименование валюты должно состоять из 3 букв"
+
+        if not vacancy_url.startswith("https://"):
+            raise "Ссылка должна начинаться с https://"
+        elif not vacancy_url.endswith(".ru"):
+            raise "Ссылка должна заканчиваться на .ru"
+        if not len(description) >= 20:
+            raise TypeError("Описание должно состоять минимум из 20 символов")
+
+        return True
 
     @staticmethod
-    def __get_user_salary(salary: str | dict) -> dict:
-        """Получает зарплату в виде "от до валюта" и возвращает в виде словаря."""
+    def __get_salary(salary: str | dict) -> str | dict:
+        """Получает зарплату в виде "от до валюта", "не указана" или словаря и возвращает в виде словаря."""
         if isinstance(salary, dict):
             return salary
-        salary = salary.split()
-        salary_dict = {
-            "from": salary[0],
-            "to": salary[1],
-            "currency": salary[2].upper(),
-        }
-        return salary_dict
+        elif salary != "Не указана":
+            salary_from, salary_to, currency = salary.split()
+            salary_dict = {
+                "from": salary_from,
+                "to": salary_to,
+                "currency": currency.upper(),
+            }
+            return salary_dict
+        else:
+            return salary
 
-    def add_user_vacancy(self):
-        """Метод для добавления пользовательской вакансии основной список вакансий"""
-        vacancy = {"id": self.vacancy_id,
-                   "name": self.name,
-                   "salary": self.salary,
-                   "vacancy_url": self.vacancy_url,
-                   "description": self.description,
-                   }
-        self.collected_vacancies.append(vacancy)
+    def add_vacancy(self):
+        """Метод для добавления пользовательской вакансии в список вакансий"""
+        # vacancy = {"id": self.vacancy_id,
+        #            "name": self.name,
+        #            "salary": self.salary,
+        #            "vacancy_url": self.vacancy_url,
+        #            "description": self.description,
+        #            }
+        self.collected_vacancies.append(self)
 
-    def get_vacancy_info(self):
-        vacancy_info = ('------------------\n'
-                        f'ID вакансии: {self.vacancy_id}\n'
-                        f'Наименование вакансии: {self.name}\n'
-                        'Зарплата: \n'
-                        f'\tот: {self.salary["from"]}\n'
-                        f'\tдо: {self.salary["to"]}\n'
-                        f'\tвалюта: {self.salary["currency"]}\n'
-                        f'Ссылка: {self.vacancy_url}\n'
-                        f'Описание: {self.description}')
-        return vacancy_info
+    def get_salary_for_print(self):
+        """Для печати зарплаты в необходимом виде."""
+        if isinstance(self.salary, dict):
+            salary = (f'\tот: {self.salary["from"]}\n'
+                      f'\tдо: {self.salary["to"]}\n'
+                      f'\tвалюта: {self.salary["currency"]}')
+            return salary
+        return self.salary
 
     def __str__(self):
         # принт вакансии
         vacancy_info = ('------------------\n'
                         f'ID вакансии: {self.vacancy_id}\n'
-                        f'Наименование вакансии: {self.name}\n')
+                        f'Наименование вакансии: {self.name}\n'
+                        f'Зарплата: \n{self.get_salary_for_print()}\n'
+                        f'Ссылка: {self.vacancy_url}\n'
+                        f'Описание: {self.description}')
         return vacancy_info
 
     def __repr__(self):
@@ -98,12 +154,8 @@ class APIVacancy(ABC):
         pass
 
 
-class HeadHunterAPI(APIVacancy, Vacancy):
-    """Класс для работы с вакансиями сайта HH.ru посредством API."""
+class HeadHunterAPI(APIVacancy, MixinVacanciesStorage):
     __slots__ = ()
-
-    def __init__(self) -> None:
-        pass
 
     def get_vacancies(self, query: str) -> list:
 
@@ -144,7 +196,7 @@ class HeadHunterAPI(APIVacancy, Vacancy):
 
     @staticmethod
     def __get_hh_salary(vacancy: dict) -> str | dict:
-        """Метод для записи зарплаты в JSON по шаблону."""
+        """Возвращает зарплату по шаблону."""
         if vacancy["salary"]:
             if vacancy["salary"]["from"]:
                 salary_from = vacancy["salary"]["from"]
@@ -178,14 +230,10 @@ class HeadHunterAPI(APIVacancy, Vacancy):
         return 'Не указано'
 
 
-class SuperJobAPI(APIVacancy, Vacancy):
+class SuperJobAPI(APIVacancy, MixinVacanciesStorage):
     """Класс для работы с вакансиями сайта superjob.ru посредством API."""
     __slots__ = ()
-
     secret_key = os.getenv('sj_key')
-
-    def __init__(self) -> None:
-        pass
 
     def get_vacancies(self, query: str) -> list:
 
@@ -194,8 +242,6 @@ class SuperJobAPI(APIVacancy, Vacancy):
         for page in range(0, 1):
             new_data = json.loads(self.__get_page(query, page))
 
-            # список для форматированных вакансий
-            formatted_vacancies = []
             for vacancy in new_data['objects']:
                 vacancy_id = int(vacancy["id"])
                 name = vacancy["profession"]
